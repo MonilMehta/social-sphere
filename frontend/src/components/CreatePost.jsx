fcfc6dff2a886b08808a2f753c79ca577712d4d9
mitanshkanani <143603367+mitanshkanani@users.noreply.github.com>
2024-05-01 19:31:15 +0530

    .

--- frontend/src/components/CreatePost.jsx
+++ frontend/src/components/CreatePost.jsx
@@ -1,18 +1,3 @@
-// import React from 'react'
-
-// const CreatePost = () => {
-//   return (
-//     <div className='create-post'>
-//       <h3>Create Post</h3>
-//       <form>
-//         <textarea placeholder='What Circling?'></textarea>
-//         <button type='submit'>Post</button>
-//       </form>
-//     </div>
-//   )
-// }
-
-// export default CreatePost
 
 import React, { useState } from "react";
 
